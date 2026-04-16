91147e4d0aec2bcde82d7aca1958fe7488c39340
manuelbamise <etobi1930@gmail.com>
2026-04-16 16:19:08 +0100

    Process: final

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,15 @@ import 'dotenv/config';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '@prisma/client';
 import { env } from 'prisma/config';
+import { PrismaLibSql } from '@prisma/adapter-libsql';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaBetterSqlite3({ url: env('DATABASE_URL') });
+// const adapter = new PrismaBetterSqlite3({ url: env('DATABASE_URL') });
+const adapter = new PrismaLibSql({
+  url: env('TURSO_DATABASE_URL'),
+  authToken: env('TURSO_AUTH_TOKEN'),
+});
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
